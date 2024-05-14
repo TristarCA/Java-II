0117d87123380057fd0e89fd7540b70a94a487c7
TristarCA <tristan.norman19@gmail.com>
2024-05-14 12:04:52 -0230

    New Semester

--- src/main/java/Assignment1/WeightConverter.java
+++ src/main/java/Assignment1/WeightConverter.java
@@ -13,7 +13,7 @@ public class WeightConverter {
 
     public static void main(String[] args) {
         WeightConverter converter = new WeightConverter();
-        System.out.println(converter.poundsToKilos(4));
+        System.out.println(converter.poundsToKilos(2));
         System.out.println(converter.kilosToPounds(4));
         System.out.println(converter.kilosToPounds(-1));
         System.out.println(converter.kilosToPounds(0));
